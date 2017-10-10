3e0856fcc4d134afefe25f95e46e0e5670445a4f
Adrien Cantérot <adrien.canterot@gmail.com>
2017-10-10 19:10:56 +0200

    🚀

---
+++ Anki/DeckImporter.swift
@@ -0,0 +1,22 @@
+//
+//  DeckImporter.swift
+//  Anki
+//
+//  Created by Adrien Cantérot on 10/10/2017.
+//  Copyright © 2017 Adrien Cantérot. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DeckImporter: UIDocumentPickerViewControllerDelegate {
+    public init() {
+
+    }
+
+    public var url: URL?
+    
+    func `import`() {
+        
+    }
+}
--- Anki/ViewController.swift
+++ Anki/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIDocumentPickerDelegate {
 
+    let deckImporter = DeckImporter()
+    
+    @IBAction func importAction(_ sender: Any) {
+        let documentPicker = UIDocumentPickerViewController(documentTypes: ["apkg"], in: .import)
+        documentPicker.delegate = self
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        if let url = urls.first {
+            self.deckImporter.url = url
+        }
     }
 
     override func didReceiveMemoryWarning() {
